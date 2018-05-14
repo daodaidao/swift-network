e028f30b3faca2556a6bfdccc19e72c11536d988
JayShang <yijieshang@gmail.com>
2018-05-14 11:01:37 +0800

    简单的AlamoFireDemo

---
+++ AlamoFireDemo(最简单的AlamoFire)/AlamoFireDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AlamoFireDemo
+//http://www.swiftv.cn/secure/course/ilm67af3/learn?lectureId=ilm67af30.31168218306265770.20555353513918817#lesson/ilm67af30.31168218306265770.20555353513918817
+//  Created by caihongguang on 2018/5/14.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+      //上面有视频地址
+        //https://github.com/Alamofire/Alamofire/blob/master/Documentation/Alamofire%204.0%20Migration%20Guide.md
+        
+   
+ 
+    
+    }
+    
+
+    
+
+}
+
